534d264f1c638379582b2599835ceaa39615042a
unknown <alyu@ucsd.edu>
2022-01-14 12:26:37 -0800

    update abilityagg schema

--- schemas/abilityagg.sql
+++ schemas/abilityagg.sql
@@ -13,17 +13,22 @@ CREATE TABLE abilityagg AS (
 		GROUP BY p.selectedrewardabilityid
 	)
 	SELECT 
+	c_cust_ab.displayname,
+	c_cust_ab.description,
 	c_cust_ab.id AS ability_id,
 	ROUND (ab_wr.wr * 100, 2) AS ability_wr,
 	ROUND (ab_ct.clear_time) * interval '1 sec' AS clear_time,
 	ab_ct.num_clears,
-	c_cust_ab.name,
-	REPLACE(c_cust_ab.abilityname, 'aghsfort_', ''),
-	c_cust_ab.displayname,
-	c_cust_ab.description
+	--c_cust_ab.name,
+	--REPLACE(REPLACE(c_cust_ab.abilityname, 'aghsfort_', ''), 'sand_king', 'sandking') AS ability_name,
+	--c_exc.localizedName AS extracted_name,
+	--c_exc.description AS extracted_description,
+	c_exc.texturename
 	FROM ability_wr ab_wr 
 	JOIN ability_clear_time ab_ct USING(selectedrewardabilityid)
 	JOIN const_customAbilites c_cust_ab ON ab_wr.selectedrewardabilityid = c_cust_ab.id
+	LEFT JOIN const_extractedAbilities c_exc USING(name)
+	WHERE c_cust_ab.name LIKE '%special%'
 	ORDER BY clear_time ASC
 );
 SELECT * FROM abilityagg LIMIT 10;
